f624078921b38c9d3ad8c570e8da2e2f1df8ddc4
GalaGrudinina <92998083+GalaGrudinina@users.noreply.github.com>
2021-11-07 23:01:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function generateCat(){
     var image=document.createElement ('img');
     var div= document.getElementById('flex-cat-gen');
-    image.src='https://cataas.com/#/cats/tags/gif/page/1';
+    image.src='https://cataas.com/cat/gif';
     div.appendChild (image);
 
 }
